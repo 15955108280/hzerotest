7dda7a2d6aeb3751d67829775d973d1182471a8c
杨星鹏 <xingpeng.yang@hand-china.com>
2019-08-29 14:33:24 +0800

    add message bundel

---
+++ src/main/java/org/hzero/test1/infra/constant/Instance.java
@@ -0,0 +1,11 @@
+package org.hzero.test1.infra.constant;
+
+/**
+ * description
+ *
+ * @author YXP 2019/08/29 9:11
+ */
+public class Instance {
+	public  static final String ERROR_HEADER_NOT_FOUND = "error.header.not_found";
+	public  static final String ERROR_LINEITEM_NOT_FOUND = "error.line_item.not_found";
+}
