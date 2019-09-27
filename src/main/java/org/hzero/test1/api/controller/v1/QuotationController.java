14acc1e36cefc205b235e159992b49bed18a58b3
杨星鹏 <xingpeng.yang@hand-china.com>
2019-09-27 15:14:52 +0800

    测试冲突dev

--- src/main/java/org/hzero/test1/api/controller/v1/QuotationController.java
+++ src/main/java/org/hzero/test1/api/controller/v1/QuotationController.java
@@ -63,7 +63,7 @@ public class QuotationController {
     public ResponseEntity<QuotationLine> submitQuotation(
                     @ApiParam(value = "租户", required = true) @PathVariable("organizationId") Long tenantId,
                     @ApiParam(value = "供应商id", required = true) @PathVariable Long supplierCompanyId,
-                    @RequestBody SubmitPriceDTO submitPriceDTO) { //测试pull的冲突
+                    @RequestBody SubmitPriceDTO submitPriceDTO) { //测试pull的冲突dev
         return Results.success(quotationService.submitQuotation(tenantId, supplierCompanyId, submitPriceDTO));
 
     }
